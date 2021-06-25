bea449f9b5800eacd6c3764abc3434913e7aac28
sinjupayyeri <44390335+sinjupayyeri@users.noreply.github.com>
2021-06-25 19:38:28 +0530

    Add files via upload

---
+++ streamlit_loan.py
@@ -0,0 +1,66 @@
+import pickle
+import streamlit as st
+ 
+# loading the trained model
+pickle_in = open('classifier.pkl', 'rb') 
+classifier = pickle.load(pickle_in)
+ 
+
+  
+# defining the function which will make the prediction using the data which the user inputs 
+def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
+ 
+    # Pre-processing user input    
+    if Gender == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+ 
+    if Married == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+ 
+    if Credit_History == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1  
+ 
+    LoanAmount = LoanAmount / 1000
+ 
+    # Making predictions 
+    prediction = classifier.predict( 
+        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+     
+    if prediction == 0:
+        pred = 'Not Eligibile'
+    else:
+        pred = 'Eligibile'
+    return pred
+      
+  
+# this is the main function in which we define our webpage  
+def main():       
+    
+    from PIL import Image
+    image = Image.open('loan_im.jpg')
+    st.image(image,use_column_width=True) 
+    st.sidebar.info('This app is created to predict whether the loan will be approved or not')
+    st.title("Predicting Loan Eligibility") 
+      
+    # following lines create boxes in which user can enter data required to make prediction 
+    Gender = st.selectbox('Gender',("Male","Female"))
+    Married = st.selectbox('Marital Status',("Unmarried","Married")) 
+    ApplicantIncome = st.number_input("Applicants monthly income") 
+    LoanAmount = st.number_input("Total loan amount")
+    Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
+    result =""
+      
+    
+    if st.button("Predict"): 
+        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
+        st.success('Your loan is {}'.format(result))
+        
+     
+if __name__=='__main__': 
+    main()
